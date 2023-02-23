57f9f09e39da830b61017d6d9f97eda15b8b88ae
jvanheesch <jvanheesch@gmail.com>
2023-02-23 12:45:08 +0100

    add todo

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -4,6 +4,7 @@ class GildedRose {
     private static final String AGED_BRIE = "Aged Brie";
     private static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
     private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    // TODO: reconsider name, as sulfurs has quality 80
     private static final int MAXIMUM_QUALITY = 50;
 
     Item[] items;

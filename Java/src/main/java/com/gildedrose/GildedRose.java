c220330d586918d43dcbb7375d93f05f8be1c068
jvanheesch <jvanheesch@gmail.com>
2023-02-23 12:47:58 +0100

    invert 'if' condition

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -15,14 +15,8 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(AGED_BRIE)
-                && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
+            if (item.name.equals(AGED_BRIE)
+                || item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
                 if (item.quality < MAXIMUM_QUALITY) {
                     item.quality = item.quality + 1;
 
@@ -38,6 +32,12 @@ class GildedRose {
                                 item.quality = item.quality + 1;
                             }
                         }
+                    }
+                }
+            } else {
+                if (item.quality > 0) {
+                    if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                        item.quality = item.quality - 1;
                     }
                 }
             }

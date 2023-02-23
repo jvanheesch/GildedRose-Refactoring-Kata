c5974747a5cb21d9ae17f15c1cda4f20f70d5d02
jvanheesch <jvanheesch@gmail.com>
2023-02-23 12:48:22 +0100

    invert 'if' condition

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -52,14 +52,14 @@ class GildedRose {
                         item.quality = item.quality + 1;
                     }
                 } else {
-                    if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                    if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                        item.quality = 0;
+                    } else {
                         if (item.quality > 0) {
                             if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
                                 item.quality = item.quality - 1;
                             }
                         }
-                    } else {
-                        item.quality = 0;
                     }
                 }
             }
